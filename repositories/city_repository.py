c29a853c05cbc62ef383dc7fde92140256bab078
Cameron Wilson <cameronwilson@Camerons-MacBook-Air.local>
2022-05-29 16:23:31 +0100

    Crud actions added for city

--- repositories/city_repository.py
+++ repositories/city_repository.py
@@ -11,3 +11,23 @@ def save(city):
     id = results[0]['id']
     city.id = id
     return city
+
+def select_all():
+    cities = []
+
+    sql = "SELECT * FROM cities"
+    results = run_sql(sql)
+
+    for row in results:
+        city = City(row['name'], row['country'],row['id'] )
+        cities.append(city)
+    return cities
+
+def delete_all():
+    sql = "DELETE  FROM cities"
+    run_sql(sql)
+
+def delete(id):
+    sql = "DELETE  FROM cities WHERE id = ?"
+    values = [id]
+    run_sql(sql, values)
