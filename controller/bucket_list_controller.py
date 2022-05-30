6206a20684eb41fcc3df5d3fca03583a35cb40f6
Cameron Wilson <cameronwilson@Camerons-MacBook-Air.local>
2022-05-30 11:34:43 +0100

    App now working

--- controller/bucket_list_controller.py
+++ controller/bucket_list_controller.py
@@ -6,7 +6,3 @@ import repositories.city_repository as city_repository
 import repositories.country_repository as country_repository
 
 bucket_list_blueprint = Blueprint("bucket_list", __name__)
-
-@bucket_list_blueprint.route("/")
-def home():
-    return render_template("books/index.html")
