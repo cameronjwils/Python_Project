6246a125b29fbf0d35aa6666809db154ed39bfb9
Cameron Wilson <cameronwilson@Camerons-MacBook-Air.local>
2022-05-30 11:24:20 +0100

    Added app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+from controller.bucket_list_controller import bucket_list_blueprint
+
+app = Flask(__name__)
+
+app.register_blueprint(bucket_list_blueprint)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
