1ab66c6cfa6e260fb73e3450beebd1e9484db996
Cameron Wilson <cameronwilson@Camerons-MacBook-Air.local>
2022-05-30 13:45:02 +0100

    Route to bucket list added

--- controller/bucket_list_controller.py
+++ controller/bucket_list_controller.py
@@ -8,6 +8,6 @@ import repositories.country_repository as country_repository
 bucket_list_blueprint = Blueprint("bucket_list", __name__)
 
 @bucket_list_blueprint.route("/bucket_list")
-def books():
-    countries = country_repository.select_all()
-    return render_template("")
+def bucket_list():
+    cities = city_repository.select_all()
+    return render_template("cities/show_cities.hmtl", all_cities = cities)
