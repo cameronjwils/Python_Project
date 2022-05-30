ce733f7b83a7ed976ed2e75a710ef85c7c54e8a7
Cameron Wilson <cameronwilson@Camerons-MacBook-Air.local>
2022-05-30 11:08:04 +0100

    Controller file added and files imported

---
+++ controller/bucket_list_controller.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+from models.city import City
+from models.country import Country
+import repositories.city_repository as city_repository
+import repositories.country_repository as country_repository
+
+bucket_list_blueprint = Blueprint("bucket_list", __name__)
